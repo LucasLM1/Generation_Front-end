9861b87b12d492f65e2676b661d29bbd836257af
LucasLM1 <lucascorreia325@gmail.com>
2021-08-20 20:35:25 -0300

    Validação de formulário com JavaScript

--- Bootstrap/js/script.js
+++ Bootstrap/js/script.js
@@ -1,76 +1,61 @@
 let nome = window.document.getElementById('nome')
-let email = document.querySelector('#email') //dentro de input chame o email
+let email = document.querySelector('input#email')
 let assunto = document.querySelector('#assunto')
-let nomeOk = false
-let emailOk = false
-let assuntoOk = false
+let nomeOk = false 
+let emailOk = false 
+let assuntoOk = false 
+let mapa = document.querySelector('#mapa')
 
+nome.style.width = '100%'
+email.style.width = '100%'
 
 function validaNome() {
+let txtNome = document.querySelector('#txtNome')
 
-    let txt = document.querySelector('#txtNome')
-    if (nome.value.length <= 1) {
-        txt.innerHTML = ""
-    }
-    else if (nome.value.length < 3) { // Obs.: vi o metódo no video da plataforma porém fiz assim pois fica melhor visivelmente
-        txt.innerHTML = "Nome inválido"
-        txt.style.color = "black" //cor da fonte
-        txt.style.fontSize = "18px" // tamanho da fonte
-        txt.style.margin = "7px"
-    }
-    else {
-        txt.innerHTML = ""
+    if (nome.value.length < 3) {
+        txtNome.innerHTML='Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML='Nome Válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
     }
 
 }
 
 function validaEmail() {
-    let txt = document.querySelector('#txtEmail')
+    let txtEmail = document.querySelector('#txtEmail') 
+ 
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ) {
+       txtEmail.innerHTML = 'E-mail inválido'
+       txtEmail.style.color = 'red'
+    } else {
+       txtEmail.innerHTML = 'E-mail válido'
+       txtEmail.style.color = 'green'
+       emailOk = true
+    }
+ }
 
-    if (email.value.length <= 1) {
-        txtEmail.innerHTML = ""
+ function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+ 
+    if (assunto.value.length >= 100) {
+       txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+       txtAssunto.style.color = 'red'
+       txtAssunto.style.display = 'block'
+    } else {
+       txtAssunto.style.display = 'none'
+       assuntoOk = true
     }
-    else if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) { // se n tiver ponto e nem @ acusa erro
-        txtEmail.innerHTML = "Email inválido"
-        txt.style.color = "black" //cor da fonte
-        txt.style.fontSize = "18px" // tamanho da fonte
-        txt.style.margin = "7px"
-    }
-    else {
-        txtEmail.innerHTML = ""
-    }
-}
-
-function validaAssunto() {
-
-    let txt = document.querySelector('#txtAssunto')
-    if (assunto.value.length <= 1) {
-        txtAssunto.innerHTML = ""
-    }
-    else if (assunto.value.length < 3) { // Obs.: vi o metódo no video da plataforma porém fiz assim pois fica melhor visivelmente
-        txtAssunto.innerHTML = "Digite um assunto válido"
-        txtAssunto.style.color = "#black" //cor da fonte
-        txtAssunto.style.fontSize = "18px" // tamanho da fonte
-        txtAssunto.style.margin = "7px"
-    }
-    else {
-        txtAssunto.innerHTML = "Você só pode digitar até 100 caracteres, caso contrario o assunto não será enviado"
-        txt.style.color = "black" //cor da fonte
-        txt.style.fontSize = "18px" // tamanho da fonte
-        txt.style.margin = "7px"
-    }
-
-}
-
-function enviar() {
+ }
+ 
+ function enviar() {
     if (nomeOk == true && emailOk == true && assuntoOk == true) {
-        alert('Formulário enviado com sucesso!')
+       alert ('Formulário enviado com sucesso!')
+    } else if (nomeOk != true && emailOk != true && assuntoOk != true) {
+       alert ('Preencha o formulário corretamente antes de enviar...')
     }
-    else if (nomeOk != true && emailOk != true && assuntoOk != true) {
-        alert('Preencha o formulário corretamente antes de enviar...')
-    }
-}
-
+ }
 
 function mapaZoom() {
     mapa.style.width = '450px'
